f0ba030393bcf4a1ec12e56b6c6b07470b529bf1
quanpa <quanpa@minder.studio>
2025-08-08 10:13:10 +0700

    update button atribute

---
+++ Assets/_GAME/Scripts/UI/PreInGame/UpgradeButtonVisual.cs
@@ -0,0 +1,34 @@
+using IPS;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeButtonVisual : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI levelText;
+    [SerializeField] private TextMeshProUGUI upgradeCost;
+    [SerializeField] private TextMeshProUGUI upgradeValue;
+    [SerializeField] private Image[] imgStates;
+    [SerializeField] private Sprite enableState, disableState;
+    private EAtribute atributeType;
+    public void Init(EAtribute type) {
+        atributeType = type;
+    }
+
+    public void ShowCurrentLevel(int currentLevel) {
+        levelText.text = currentLevel.ToString();
+        if (!GameData.Instance.AtributesData.TryGetAtribute(atributeType, out var atribute)) return;
+        upgradeValue.text = atribute.GetValue(currentLevel).ToString();
+        upgradeCost.text = atribute.GetCost(currentLevel).ToString();
+        if (imgStates == null || imgStates.Length <= 0) return;
+        var current = currentLevel % imgStates.Length;
+        if (current == 0)
+            current = imgStates.Length;
+        for (int i = 0; i < imgStates.Length; i++) {
+            if (i < current)
+                imgStates[i].sprite = enableState;
+            else
+                imgStates[i].sprite = disableState;
+        }
+
+    }
+}
