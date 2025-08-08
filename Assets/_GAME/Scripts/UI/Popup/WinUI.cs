d9ed766a4e2b60cb47555368bf9c9dc37f7a11aa
quanpa <quanpa@minder.studio>
2025-08-08 11:23:01 +0700

    update

--- Assets/_GAME/Scripts/UI/Popup/WinUI.cs
+++ Assets/_GAME/Scripts/UI/Popup/WinUI.cs
@@ -4,17 +4,26 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class WinUI : Frame
-{
+public class WinUI : Frame {
     [SerializeField] TextMeshProUGUI textDistance;
     [SerializeField] private ButtonEffect bntClaim;
-    private void OnEnable() {
+    [SerializeField] private TextMeshProUGUI textClaim;
+    [SerializeField] Transform tartGetCollect;
+    private int claimCoin;
+    private void OnEnable() {      
         bntClaim.AddListener(OnClickClaim);
     }
+
     private void OnClickClaim() {
-        ItemCollectEffect.Instance.Collect()
+        ItemCollectEffect.Instance.Collect(textClaim.transform.position, tartGetCollect, () => {
+            UserData.SetCoin(UserData.CurrentCoin + (ulong)claimCoin);
+        }, () => {
+            //LevelManager.in
+        });
     }
     public void SetTextDistance(float distance) {
         textDistance.SetText($"Km : {Mathf.RoundToInt(distance)}");
+        claimCoin = Mathf.FloorToInt(distance * GameData.Instance.GameConfig.BaseClaim * GameData.Instance.AtributesData.GetValue(EAtribute.Income, UserData.CurrentLevel));
+        textClaim.text = claimCoin.ToString();
     }
 }
